cdc1f6e20df36726182ac0f2abb86bf7b52dce5f
Iftakher Hossen <iftakher.one@gmail.com>
2022-06-08 20:00:19 +0600

    fixing cors error

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
+
 // middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(fileUpload());
 
