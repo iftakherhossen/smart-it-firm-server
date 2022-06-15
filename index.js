f90ac0743e07c59d342d30f056e0fe5b29793dab
Iftakher Hossen <iftakher.one@gmail.com>
2022-06-15 18:51:39 +0600

    trying 5

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use((req, res, next) => {
-    res.header({ "Access-Control-Allow-Origin": "*", mode: "no-cors" });
+    res.header({ "Access-Control-Allow-Origin": "*" });
     next();
 }) 
 app.use(express.json());
