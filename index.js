a02a36e59023af76c4c812a1023f83d390df2c36
Iftakher Hossen <iftakher.one@gmail.com>
2022-06-15 18:46:38 +0600

    trying 4

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(express.json());
-app.use(fileUpload());
 app.use((req, res, next) => {
     res.header({ "Access-Control-Allow-Origin": "*", mode: "no-cors" });
     next();
 }) 
+app.use(express.json());
+app.use(fileUpload());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wk6ov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
