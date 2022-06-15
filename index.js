b37049f6d97f7699df469b05520f7af046a8880e
Iftakher Hossen <iftakher.one@gmail.com>
2022-06-15 18:11:46 +0600

    trying 1

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 app.use((req, res, next) => {
-    res.header({"Access-Control-Allow-Origin": "*"});
+    res.header({ "Access-Control-Allow-Origin": "*", mode: "no-cors" });
     next();
 }) 
 
