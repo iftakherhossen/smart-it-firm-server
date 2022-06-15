4a8b59bfdf1aa0a6af45297f1f4a4758deb43f2b
Iftakher Hossen <iftakher.one@gmail.com>
2022-06-15 18:45:46 +0600

    trying 3

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 app.use((req, res, next) => {
-    res.header({ "Access-Control-Allow-Origin": "https://smart-it-firm-server.herokuapp.com/", mode: "no-cors" });
+    res.header({ "Access-Control-Allow-Origin": "*", mode: "no-cors" });
     next();
 }) 
 
